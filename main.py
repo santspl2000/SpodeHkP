39fa4a0e668b785f5bd30cb8f01510d34cc1f96f
Sant_2000 <155839052+santspl2000@users.noreply.github.com>
2024-02-21 20:18:46 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+def LocalHkPAV(list2: List[any]):
+    global LocalLFI
+    LocalLFI = "Local" + "HackingP"
+LocalLFI = ""
+LocalHkP = 10 * 30
+led.plot(5, 5)
+basic.show_icon(IconNames.CHESSBOARD)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,10 @@
+function LocalHkPAV (list: any[]) {
+    LocalLFI = "Local" + "HackingP"
+}
+let LocalLFI = ""
+let LocalHkP = 10 * 30
+led.plot(5, 5)
+basic.showIcon(IconNames.Chessboard)
+basic.forever(function () {
+	
+})
